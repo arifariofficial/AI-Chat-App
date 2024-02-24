04f660d4b57230d5ec36b708284054eda53e589d
Ariful Islam <ariful6@yahoo.com>
2024-02-24 21:30:47 +0200

    query added 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
+"use client";
+import Questionnaire from "@components/Query";
+
 const Home = () => {
+  const handleQuestionnaireCompletion = (answers: {
+    hasInsurance: string;
+    happenedInFreeTime: string;
+  }) => {
+    console.log("Questionnaire Answers:", answers);
+  };
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Legal AI Assistant
-        <br />
-        <span className="orange_gradient text-3xl">
-          Easing legal processes and counseling through AI
-        </span>
-      </h1>
-      <p className="desc text-center">
-        Counseling for the average person and high focus on the individual&apos;s rights in
-        insurance cases
-      </p>
-    </section>
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Legal AI Assistant
+          <br />
+          <span className="orange_gradient text-3xl">
+            Easing legal processes and counseling through AI
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Counseling for the average person and high focus on the individual&apos;s rights in
+          insurance cases
+        </p>
+      </section>
+      <Questionnaire onCompletion={handleQuestionnaireCompletion} />
+    </>
   );
 };
 
