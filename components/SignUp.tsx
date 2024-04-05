c58a07051ae6e95184e872f63c998c015f46f5b4
Ariful Islam <ariful6@yahoo.com>
2024-04-06 00:21:04 +0300

    fix-api-singup

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -14,7 +14,7 @@ const SignupPage = () => {
     setMessage(""); // Reset message
 
     try {
-      const response = await fetch("http://localhost:3000/api/signup", {
+      const response = await fetch("/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
