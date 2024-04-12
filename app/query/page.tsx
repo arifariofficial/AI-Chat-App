0de79b20c612f09bfb16140498539e0c4201850e
Ariful Islam <ariful6@yahoo.com>
2024-04-12 03:12:44 +0300

    bug

--- app/query/page.tsx
+++ app/query/page.tsx
@@ -1,6 +1,4 @@
 import Questionary from "@components/Questionary";
-import { ThemeProvider } from "@mui/material";
-import theme from "@providers/theme";
 
 export default function QuestionaryComponent() {
   return (
