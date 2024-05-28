65298fcdaeb3c8497badbe32162d2f8e8b26fdd8
Ariful Islam <ariful6@yahoo.com>
2024-05-28 08:49:08 +0300

    adminer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-# Run Prisma db push to update the database schema
-echo "Running npx prisma db push..."
-npx prisma db push
+# Check if we are in a development environment
+if [ "$NODE_ENV" = "development" ]; then
+    echo "Running npx prisma db push..."
+    npx prisma db push || {
+        echo "Failed to update the database schema with Prisma db push."
+        exit 1
+    }
+else
+    echo "Running npx prisma migrate deploy..."
+    npx prisma migrate deploy || {
+        echo "Failed to deploy migrations using Prisma migrate."
+        exit 1
+    }
+fi
 
 # Then start your application
 echo "Starting the application..."
